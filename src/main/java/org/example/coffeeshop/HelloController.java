c1e7458d8d3426d37849bad1e0be6314938d92f3
Leo-Maximus <leo.longlhb@gmail.com>
2024-07-16 19:10:20 +0700

    first commit

---
+++ src/main/java/org/example/coffeeshop/HelloController.java
@@ -0,0 +1,86 @@
+package org.example.coffeeshop;
+
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class HelloController implements Initializable {
+    @FXML
+    private Hyperlink si_forgot;
+
+    @FXML
+    private Button si_loginBtn;
+
+    @FXML
+    private AnchorPane si_loginForm;
+
+    @FXML
+    private PasswordField si_password;
+
+    @FXML
+    private TextField si_username;
+
+    @FXML
+    private Button side_CreateBtn;
+
+    @FXML
+    private AnchorPane side_form;
+
+    @FXML
+    private TextField su_answer;
+
+    @FXML
+    private PasswordField su_password;
+
+    @FXML
+    private ComboBox<?> su_question;
+
+    @FXML
+    private Button su_signupBtn;
+
+    @FXML
+    private AnchorPane su_signupForm;
+
+    @FXML
+    private TextField su_username;
+
+    @FXML
+    private Button side_alreadyHave;
+
+    //login form <-> register form
+    public void switchForm(ActionEvent event) {
+        TranslateTransition slider = new TranslateTransition();
+        if (event.getSource() == side_CreateBtn) {
+            slider.setNode(side_form);
+            slider.setToX(300);
+            slider.setDuration(Duration.seconds(.5));
+            slider.setOnFinished((ActionEvent e) -> {
+                side_alreadyHave.setVisible(true);
+                side_CreateBtn.setVisible(false);
+            });
+            slider.play();
+        } else if (event.getSource() == side_alreadyHave) {
+            slider.setNode(side_form);
+            slider.setToX(0);
+            slider.setDuration(Duration.seconds(.5));
+            slider.setOnFinished((ActionEvent e) -> {
+                side_alreadyHave.setVisible(false);
+                side_CreateBtn.setVisible(true);
+            });
+            slider.play();
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
