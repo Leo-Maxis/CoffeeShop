f4bba2ce611e243996d4b02df5f35211a7cd7a43
Leo-Maximus <leo.longlhb@gmail.com>
2024-07-30 19:24:08 +0700

    add card product form

---
+++ src/main/java/org/example/controller/CardProductController.java
@@ -0,0 +1,38 @@
+package org.example.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CardProductController implements Initializable {
+
+    @FXML
+    private AnchorPane card_form;
+
+    @FXML
+    private Button prod_addBtn;
+
+    @FXML
+    private ImageView prod_imageView;
+
+    @FXML
+    private Label prod_name;
+
+    @FXML
+    private Label prod_price;
+
+    @FXML
+    private Spinner<?> prod_spinner;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
